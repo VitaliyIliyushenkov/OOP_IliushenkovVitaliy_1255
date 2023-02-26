6723b54e8f2aec4fc89fbabaa3241f93cfb8812c
Vitaliy <ivi.22@uni-dubna.ru>
2023-02-26 16:03:24 +0300

    add new file

---
+++ WindowsFormsApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    internal static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {   
+            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
