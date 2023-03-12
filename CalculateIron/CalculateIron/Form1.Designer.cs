da966655b99fa22ae079e580a71bed52576eab1a
Vitaliy <ivi.22@uni-dubna.ru>
2023-03-12 19:43:28 +0300

    New packs

---
+++ CalculateIron/CalculateIron/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CalculateIron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numerator = new System.Windows.Forms.Label();
+            this.denominator = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "________________";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(248, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button2.Location = new System.Drawing.Point(248, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button3.Location = new System.Drawing.Point(248, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button4.Location = new System.Drawing.Point(248, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = " /";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "________________";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(305, 222);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(305, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(419, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 26);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "=";
+            // 
+            // numerator
+            // 
+            this.numerator.AutoSize = true;
+            this.numerator.Location = new System.Drawing.Point(467, 185);
+            this.numerator.Name = "numerator";
+            this.numerator.Size = new System.Drawing.Size(54, 13);
+            this.numerator.TabIndex = 11;
+            this.numerator.Text = "numerator";
+            // 
+            // denominator
+            // 
+            this.denominator.AutoSize = true;
+            this.denominator.Location = new System.Drawing.Point(466, 219);
+            this.denominator.Name = "denominator";
+            this.denominator.Size = new System.Drawing.Size(65, 13);
+            this.denominator.TabIndex = 12;
+            this.denominator.Text = "denominator";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(448, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.denominator);
+            this.Controls.Add(this.numerator);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label numerator;
+        private System.Windows.Forms.Label denominator;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CalculateIron/CalculateIron/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateIron
+{
+    public partial class Form1 : Form
+    {
+        int[] result = new int[2];
+        int del;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+                textBox2.Text = "Error";
+            else if (textBox4.Text == "0")
+                textBox4.Text = "Error";
+            else
+            {
+                result[0] = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox4.Text)
+                                + Convert.ToInt32(textBox3.Text) * Convert.ToInt32(textBox2.Text);
+                result[1] = Convert.ToInt32(textBox2.Text) * Convert.ToInt32(textBox4.Text);
+                do
+                {
+                    del = Euclids_algorithm(result[0], result[1]);
+                    result[0] /= del;
+                    result[1] /= del;
+                }
+                while (del != 1);
+                numerator.Text = result[0].ToString();
+                denominator.Text = result[1].ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+                textBox2.Text = "Error";
+            else if (textBox4.Text == "0")
+                textBox4.Text = "Error";
+            else
+            {
+                result[0] = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox4.Text)
+                                    - Convert.ToInt32(textBox3.Text) * Convert.ToInt32(textBox2.Text);
+                result[1] = Convert.ToInt32(textBox2.Text) * Convert.ToInt32(textBox4.Text);
+                do
+                {
+                    del = Euclids_algorithm(result[0], result[1]);
+                    result[0] /= del;
+                    result[1] /= del;
+                }
+                while (del != 1);
+                numerator.Text = result[0].ToString();
+                denominator.Text = result[1].ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+                textBox2.Text = "Error";
+            else if (textBox4.Text == "0")
+                textBox4.Text = "Error";
+            else
+            {
+                result[0] = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox3.Text);
+                result[1] = Convert.ToInt32(textBox2.Text) * Convert.ToInt32(textBox4.Text);
+                do
+                {
+                    del = Euclids_algorithm(result[0], result[1]);
+                    result[0] /= del;
+                    result[1] /= del;
+                }
+                while (del != 1);
+                numerator.Text = result[0].ToString();
+                denominator.Text = result[1].ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+                textBox2.Text = "Error";
+            else if (textBox4.Text == "0")
+                textBox4.Text = "Error";
+            else
+            {
+                result[0] = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox4.Text);
+                result[1] = Convert.ToInt32(textBox2.Text) * Convert.ToInt32(textBox3.Text);
+                do
+                {
+                    del = Euclids_algorithm(result[0], result[1]);
+                    result[0] /= del;
+                    result[1] /= del;
+                }
+                while (del != 1);
+                numerator.Text = result[0].ToString();
+                denominator.Text = result[1].ToString();
+            }
+        }
+        static int Euclids_algorithm(int numerator, int denominator)
+        {
+            int max;
+            int min;
+            int ost;
+
+            if (numerator > denominator)
+            {
+                max = numerator; min = denominator;
+            }
+            else
+            {
+                max = denominator; min = numerator;
+
+            }
+
+            if (max % min == 0)
+            {
+                ost = min;
+            }
+            else
+            {
+                ost = max % min;
+                int ost2 = min % ost;
+                while (ost2 != 0)
+                {
+                    int p = ost;
+                    ost = ost2;
+                    ost2 = p % ost2;
+                }
+            }
+            return ost;
+        }
+    }
+}
